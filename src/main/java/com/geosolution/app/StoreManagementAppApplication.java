eedbdfc836b537337ba461a689fb971082c8f837
Gashaw2017 <gashumengistu@gmail.com>
2019-08-13 01:53:50 -0500

    first commit

---
+++ src/main/java/com/geosolution/app/StoreManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.geosolution.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoreManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoreManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/geosolution/app/StoreManagementAppApplicationTests.java
@@ -0,0 +1,17 @@
+package com.geosolution.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+@SpringBootTest
+public class StoreManagementAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
